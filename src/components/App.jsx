deac41bb48f4721947b6f47c6007b79941dd9b8a
Jonathan Yudi Gunawan <jonathan.gunawan@xtremax.com>
2020-06-24 15:49:11 +0700

    add search function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,34 +1,30 @@
-import React from "react";
-import { Grid, Card, CardContent, Typography } from "@material-ui/core";
-import SearchBar from "./SearchBar.jsx";
+import React from "react"
+import SearchBar from "./SearchBar.jsx"
+import Body from "./Body.jsx"
 
-const words = [
-  "fee",
-  "fi",
-  "fo",
-  "fum",
-  "fee",
-  "fi",
-]
 
-function App() {
-  return (
-    <div>
-      <SearchBar />
-    	<Grid container spacing = { 3 } > {
-          words.map(word =>
-            <Grid item xs={12} sm={12/words.length}>
-              <Card>
-                <CardContent>
-                  <Typography>{word}</Typography>
-                </CardContent>
-              </Card>
-            </Grid>
-          )
-        }
-    	</Grid>
-    </div>
-  )
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      startID: null,
+    }
+  }
+
+  handleSubmit(e) {
+    this.setState({
+      startID: e.target.value,
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar onChange={(e) => this.handleSubmit(e)}/>
+        <Body startID={this.state.startID}/>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,12 +1,8 @@
 import React from "react";
 import TextField from '@material-ui/core/TextField';
 
-function SearchBar() {
-  return (
-    <form noValidate autoComplete="off">
-	  <TextField id="outlined-basic" label="ID" helperText="Enter valid ID (1-180), ex: 8, 120, ..." variant="outlined" />
-	</form>
-  )
+function SearchBar(props) {
+  return <TextField id="outlined-basic" label="ID" onChange={props.onChange} helperText="Enter valid ID (1-180), ex: 8, 120, ..." variant="outlined" />
 }
 
 export default SearchBar;
